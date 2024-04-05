4e8edf7ce22406c845a08dd6051fc594f8b15012
kikedare78@gmail.com <kikedare78@gmail.com>
2024-04-05 21:56:20 +0100

    Done

--- exercises/flow-control/src/main/java/com/cbfacademy/FlowControlExercises.java
+++ exercises/flow-control/src/main/java/com/cbfacademy/FlowControlExercises.java
@@ -1,5 +1,6 @@
 package com.cbfacademy;
 
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -9,22 +10,55 @@ public class FlowControlExercises {
     public List<String> fizzBuzz(List<Integer> numbers) {
         // TODO - Implement this method such that
         //  it creates a list where for each element of the input list ${numbers}
-        //  - if the ${element} is divisible by 3, it adds “Fizz” to the list
-        //  - if the ${element} is divisible by 5, it adds “Buzz” to the list
+        // - if the ${element} is divisible by 3, it adds “Fizz” to the list
+        // - if the ${element} is divisible by 5, it adds “Buzz” to the list  
         //  - if the ${element} is divisible by both 3 and 5, it adds “FizzBuzz” to the list
-        //  - it adds the element to the list in any other case
-        //  - it returns the constructed list
-        throw new RuntimeException("Not implemented");
+       //  - it adds the element to the list in any other case
+        List<String> fizzBuzz = new ArrayList<>();
+       
+        for (int i= 0; i < numbers.size(); i++) {
+        if (i % 3 == 0) {
+            System.out.print(i + "Fizz");
+        
+        } else if (i % 5 == 0) {
+            System.out.print(i + "Buzz");
+        
+        }else if (i % 3 == 0 && i % 5 == 0){ 
+        System.out.print(i + "FizzBuzz"); 
+
+        } else {
+        System.out.print(numbers.get(i));
+        }
     }
+        //  - it returns the constructed list
+
+        return fizzBuzz;
+    }
+    
 
     public String whichMonth(Integer number) {
+        String [] months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        Integer [] number = [1,2,3,4,5,6,7,8,9,10,11,12]
+        if (int i = 1; i >= 12) {
+           System.out.print(months[number]); 
+        } else {     
+        }
         // TODO - Implement this method such that
         //  - it returns the month corresponding to the input ${number}
         //  - if the ${number} is invalid, the method should return "Invalid month number"
-        throw new RuntimeException("Not implemented");
+        return "Invalid month number";
     }
 
     public Map<String, Integer> sumOfOddsAndSumOfEvens() {
+        Integer [] number = [1..100];
+        for (int i = 1; i >= number.length(); i++) {
+            if (int % 2 == 0);
+            System.out.print()
+
+        }
+        
+
+
         // TODO - Implement this method such that
         //  - creates and initialises a list of 100 numbers - from 1 to 100
         //  - determines the sum of all the even numbers in the list
--- exercises/java-collections/src/main/java/com/cbfacademy/CollectionsExercises.java
+++ exercises/java-collections/src/main/java/com/cbfacademy/CollectionsExercises.java
@@ -5,51 +5,103 @@ import java.util.*;
 public class CollectionsExercises {
 
     public LinkedList<Integer> useLinkedList() {
+        
         // TODO: create an empty linked list
+
+        LinkedList<Integer> integer = new LinkedList<>();
+
         //  - add 4 as the first element of the list
+        integer.add(4);
         //  - then add 5, 6, 8, 2, 9 to the list
+        integer.add(5);
+        integer.add(6);
+        
+        integer.add(8);
+        integer.add(2);
+        integer.add(9);
+        integer.add(2);
+
         //  - add another 2 as the last element of the list
+        
         //  - add 4 as the 3rd element of the list
+        integer.add(2, 4);
         //  - invoke the method element() on the list and print the result on the screen
+
+        System.out.print(integer.element());
         //  - return the list
-        throw new RuntimeException("Not implemented");
+        return integer;
     }
 
     public Stack<Integer> useStack() {
         // TODO: create an empty stack
+        
+            Stack<Integer> integer = new Stack<>();
+    
         //  - add 5, 6, 8, 9 to the stack
+            integer.add(5);
+            integer.add(6);
+            integer.add(8);
+            integer.add(2);
+            integer.add(9);
+        
         //  - print the first element of the stack on the screen
+        System.out.print(integer.firstElement());
+        
         //  - print the last element of the stack on the screen
+        System.out.print(integer.lastElement());
         //  - invoke the method pop() on the stack and print the result on the screen
+        System.out.print(integer.pop());
         //  - invoke the push(4) method on the stack
+        System.out.print(integer.push(4));
         //  - return the stack
-        throw new RuntimeException("Not implemented");
+        return integer;
     }
 
     public ArrayDeque<Integer> useArrayDeque() {
         // TODO: create an empty arrayDeque
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+        
         //  - add 5, 6, 8, 9 to the queue
+        arrayDeque.add(5);
+        arrayDeque.add(6);
+        arrayDeque.add(8);
+        arrayDeque.add(9);
+        
         //  - print the first element of the queue on the screen
+        System.out.print(arrayDeque.peekFirst());
         //  - print the last element of the queue on the screen
+        System.out.print(arrayDeque.peekLast());
         //  - invoke the method poll() on the queue and print the result on the screen
+        System.out.print(arrayDeque.poll());
         //  - invoke the element() method on the queue and print the result on the screen
+        System.out.print(arrayDeque.element());
         //  - return the queue
-        throw new RuntimeException("Not implemented");
+        return arrayDeque;
     }
 
     public HashMap<Integer, String> useHashMap() {
         // TODO: create an empty hash map
+        public HashMap<Integer, String> hashMap = new HashMap<>(); // use capaital H and got errors so changed to h and the rest of the list
         //  - add {1, TypeScript} entry to the map
+        hashMap.put(1, "TypeScript");
         //  - add {2, Kotlin} entry to the map
+        hashMap.put(2, "Kotlin");
         //  - add {3, Python} entry to the map
+        hashMap.put(3, "Phyton");
         //  - add {4, Java} entry to the map
+        hashMap.put(4, "Java");
         //  - add {5, JavaScript} entry to the map
+        hashMap.put(5, "JavaScript");
         //  - add {6, Rust} entry to the map
+        hashMap.put(6, "Rust");
         //  - determine the set of keys from the map and print it on the screen
+        System.out.print(hashMap.keySet());
         //  - determine the set of values from the map and print it on the screen
+        System.out.print(hashMap.values());
         //  - determine whether the map contains "English" as a language and print the result on the screen
+        System.out.print(hashMap.containsValue("English"));
         //  - return the map
-        throw new RuntimeException("Not implemented");
+       return hashMap;
     }
 
     public String getName() {
