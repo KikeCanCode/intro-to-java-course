c84198aab29028c1d8cd1e6646770bbeb95b3aca
kikedare78@gmail.com <kikedare78@gmail.com>
2024-04-12 22:06:15 +0100

    Test Successfull - Completed

--- exercises/flow-control/src/main/java/com/cbfacademy/FlowControlExercises.java
+++ exercises/flow-control/src/main/java/com/cbfacademy/FlowControlExercises.java
@@ -2,116 +2,138 @@ package com.cbfacademy;
 
 import java.time.Month;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 public class FlowControlExercises {
 
     public List<String> fizzBuzz(List<Integer> numbers) {
-        // TODO - Implement this method such that
-        //  it creates a list where for each element of the input list ${numbers}
-        // - if the ${element} is divisible by 3, it adds “Fizz” to the list
+           // Implement this method such that
+          //  it creates a list where for each element of the input list ${numbers}  
+         // - if the ${element} is divisible by 3, it adds “Fizz” to the list
         // - if the ${element} is divisible by 5, it adds “Buzz” to the list  
-        //  - if the ${element} is divisible by both 3 and 5, it adds “FizzBuzz” to the list
-       //  - it adds the element to the list in any other case
-        List<String> fizzBuzz = new ArrayList<>();
-       
-        for (int i= 0; i < numbers.size(); i++) {
-        if (i % 3 == 0) {
-            System.out.print(i + "Fizz");
+       // - if the ${element} is divisible by both 3 and 5, it adds “FizzBuzz” to the list
         
-        } else if (i % 5 == 0) {
-            System.out.print(i + "Buzz");
-        
-        }else if (i % 3 == 0 && i % 5 == 0){ 
-        System.out.print(i + "FizzBuzz"); 
+       List<String> fizzBuzzList = new ArrayList<String>();
+        for (Integer number : numbers) {
+            if ((number % 3 == 0) && (number % 5 == 0)){ 
+                fizzBuzzList.add("FizzBuzz"); 
 
+        } else if (number % 3 == 0) {
+                fizzBuzzList.add("Fizz");
+
+        } else if (number % 5 == 0) {
+                fizzBuzzList.add("Buzz");    
         } else {
-        System.out.print(numbers.get(i));
+       // - it adds the element to the list in any other case
+
+                fizzBuzzList.add(number.toString());
         }
     }
         //  - it returns the constructed list
 
-        return fizzBuzz;
+        return fizzBuzzList;
     }
     
 
-    //public String whichMonth(Integer number) {
-    // TODO - Implement this method such that
-    public void whichMonth(Integer number) {
-     //  - it returns the month corresponding to the input ${number}
-     //  - if the ${number} is invalid, the method should return "Invalid month number"
-        String [] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-        //int [] i  = {1,2,3,4,5,6,7,8,9,10,11,12};
-        // try with map
-
-        for (int i = 0; i <= 12; i++) {
-            if (i <=12){
-
-                System.out.print(months); 
-            } else {
-                System.out.print("Invalid month number");
-
-            }
-    
-        }       
+    public String whichMonth(Integer number) {
+        // - Implement this method such that
         
-
-    }
-    
-
-   public Map<String, Integer> sumOfOddsAndSumOfEvens() {
-    // TODO - Implement this method such that
-
-    //  - creates and initialises a list of 100 numbers - from 1 to 100
-        Integer [] number = {100};
-    //  - determines the sum of all the even numbers in the list
-        Integer sumOfOdds = 0; 
-
-        Integer sumOfEvens = 0;
-
-        for (int i = 1; i <= 100; i++) {
-            if (i % 2 == 0) { 
-            System.out.print(i+=i);
-    //  - determines the sum of all the odd numbers in the list   
-
-        } else if (i % 3 == 0) {
-
-        System.out.print(i+=i);
-    //  - returns a map with two entries:
-        //      {"SumOfEvens", calculatedSumOfEvens}, {"SumOfOdds", calculatedSumOfOdds}
-
+        //  - it returns the month corresponding to the input ${number}
+        String month;
+        switch (number){
+            
+            case 1:
+                month = "January";
+                 break;
+            case 2:
+                month = "February";
+                break;
+            case 3:
+                month = "March";
+                break;    
+            case 4:
+                month = "April";
+                 break;
+            case 5:
+                month = "May";
+                break;
+            case 6:
+                month = "June";
+                break;
+            case 7:
+                 month = "July";
+                 break;
+            case 8:
+                 month = "August";
+            break;
+                case 9:
+                month = "September";
+            break;
+                case 10:
+                month = "October";
+            break;
+                case 11:
+                month = "November";
+                 break;
+            case 12:
+                month = "December";
+                break;
+            default:
+                month = "Invalid month number";
+                break;
         }
     
-        return 
+        //  - if the ${number} is invalid, the method should return "Invalid month number"
+        return month;
+    }
+        
+    public Map<String, Integer> sumOfOddsAndSumOfEvens() {
+    // - Implement this method such that
+    //  - creates and initialises a list of 100 numbers - from 1 to 100
+    Map<String, Integer> sums = new HashMap<>();
+    List<Integer> hundred_numbers = IntStream.rangeClosed(1, 100).boxed().toList();
+    Integer sumOfEvens = 0, sumOfOdds = 0;
+    //  - determines the sum of all the even numbers in the list      
+    //  - determines the sum of all the odd numbers in the list
+    
+        for (Integer number : hundred_numbers) {
+            if(number % 2 == 0){ 
+               sumOfEvens += number;
+            }
+            else{
 
-        //)"sumOfEvens".calculatedSumOfEvens, "sumOfOdds".calculatedSumOfOdds;
+                sumOfOdds += number;
+            }
+        }
+      
+        sums.put("SumOfEvens", sumOfEvens);
+        sums.put("SumOfOdds" , sumOfOdds);
+
+    //  - returns a map with two entries
+    //      {"SumOfEvens", calculatedSumOfEvens}, {"SumOfOdds", calculatedSumOfOdds}     
+    
+        return sums;
     }
 
-    
-    public List<Integer> reverse(ArrayList<Integer> numbers) {
-        
-        // TODO - Implement this method such that
-        ArrayList<Integer> ArrayList = new ArrayList<>(numbers);
 
+    public List<Integer> reverse(ArrayList<Integer> numbers) {
+        // - Implement this method such that
         //  - it takes an array list of integers
+        ArrayList<Integer> reversed = new ArrayList<>(numbers);
         //  - it returns the list in reverse order
-        throw new RuntimeException("Not implemented");
+        Collections.reverse(reversed);
+
+         return reversed;
     }
 
     public String getName() {
         return "Flow Control Exercises";
     }
 }
-/*  public List<Integer> reverse(ArrayList<Integer> numbers) {
-        // Create a new ArrayList to store the reversed numbers
-        ArrayList<Integer> reversed = new ArrayList<>(numbers);
 
-        // Reverse the list using Collections.reverse() method
-        Collections.reverse(reversed);
-
-        // Return the reversed list
-        return reversed;
-    }
-}*/
+    
