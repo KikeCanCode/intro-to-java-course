d0d83dd34a45a53626bf001bfb5416b3aaf5d302
kikedare78@gmail.com <kikedare78@gmail.com>
2024-04-12 22:09:03 +0100

    Done

--- exercises/flow-control/src/main/java/com/cbfacademy/App.java
+++ exercises/flow-control/src/main/java/com/cbfacademy/App.java
@@ -2,7 +2,7 @@ package com.cbfacademy;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Good evening!");
-        FlowControlExercises.whichMonth(3);
+        System.out.println("Hello World!");
+       
     }
 }
