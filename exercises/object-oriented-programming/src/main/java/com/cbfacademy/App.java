ae7e23ffdf24a6ba6a9305cc0b880258a4df13b3
kikedare78@gmail.com <kikedare78@gmail.com>
2024-04-18 10:38:16 +0100

    Fixed

--- exercises/object-oriented-programming/src/main/java/com/cbfacademy/App.java
+++ exercises/object-oriented-programming/src/main/java/com/cbfacademy/App.java
@@ -1,7 +1,13 @@
 package com.cbfacademy;
 
+import com.cbfacademy.cars.Showrom;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
+
+//In the main method of the App class, instantiate a Showroom object, call getCars() and print the details for each Car instance in the list.
+        Showrom showrom = new Showrom();
+        System.out.println(showrom.getCars());
     }
 }
