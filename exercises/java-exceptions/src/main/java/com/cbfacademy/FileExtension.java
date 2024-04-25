b0cf40142aa835cd8bb001cb9de8854513c8297c
kikedare78@gmail.com <kikedare78@gmail.com>
2024-04-25 22:02:05 +0100

    Completed

--- exercises/java-exceptions/src/main/java/com/cbfacademy/FileExtension.java
+++ exercises/java-exceptions/src/main/java/com/cbfacademy/FileExtension.java
@@ -1,5 +1,67 @@
 package com.cbfacademy;
+/* Create a class called FileExtension with the following methods:
+
+boolean check(String filename)
+Map<String, int> map(List<String> filenames)
+The check method should:
+
+return true when the file extension is .java
+return false when the file extension is not .java 
+throw a custom checked exception called FilenameException (that you must create) when the file name is null or an empty string.
+The map method should:
+
+check each provided file's extension and map the returned value as 1 if true or 0 if false
+map -1 when an exception occurs*/
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class FileExtension {
 
-}
+    public boolean check (String filename) throws FilenameException {
+        
+        if (filename == null || filename.isEmpty()){ // || represent OR
+
+        throw new FilenameException( "File name is not java");
+    
+        } 
+        else if(filename.endsWith(".java")){
+            return true;
+        } 
+        else {
+            return false;
+        }
+
+        }
+        /*check each provided file's extension and map the returned value as 1 if true or 0 if falsemap -1 when an exception occurs
+        The map method should:
+        check each provided file's extension and map the returned value as 1 if true or 0 if false
+        map -1 when an exception occur*/
+
+        public Map<String, Integer> map(String filenames) {
+            Map<String, Integer> filenamesMap = new HashMap<>();
+
+           try{ 
+            for (String string: filenames){
+            //Map<String, Integer> filenamesMap;
+            if (check(string) == true){
+                filenamesMap.put(string, 1);
+            }
+            else{
+                filenamesMap.put(string, 0);
+            }
+        }
+           }
+            catch (FilenameException e) {
+                Map<String, Integer> result;
+                result.put(filenames, -1);
+                }
+            }
+        }
+
+
+    
+
+    
+        
--- exercises/java-exceptions/src/main/java/com/cbfacademy/FilenameException.java
+++ exercises/java-exceptions/src/main/java/com/cbfacademy/FilenameException.java
@@ -1,9 +1,46 @@
 package com.cbfacademy;
+/* Custom Exceptions
+Create a custom (checked) exception class called FilenameException.
+
+Create a class called FileExtension with the following methods:
+
+boolean check(String filename)
+Map<String, int> map(List<String> filenames)
+The check method should:
+
+return true when the file extension is .java
+return false when the file extension is not .java 
+throw a custom checked exception called FilenameException (that you must create) when the file name is null or an empty string.
+The map method should:
+
+check each provided file's extension and map the returned value as 1 if true or 0 if false
+map -1 when an exception occurs*/
 
 public class FilenameException extends Exception {
+
     private final String message;
 
-    public FilenameException (String message){
-        this.message = new message;
+    //private message message;
 
-        }
+    public FilenameException (String message){ 
+
+        //super(message);
+        this.message = message;
+
+    }
+
+
+
+@Override
+public String getMessage(){
+    return String.join( " - ", super.getMessage());
+}
+}
+
+
+// 
+//solve or build?
+// conception
+//steps 
+// system somewhere, and my system will valid if the file has the right extension or not. if java true or false if not? implementation details
+// 
