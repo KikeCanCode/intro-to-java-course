160f9839ac9a19e35778ac1868119a006e5d67c4
kikedare78@gmail.com <kikedare78@gmail.com>
2024-04-30 21:53:37 +0100

    Ok

---
+++ exercises/client-server-architecture/src/main/java/com/cbfacademy/WebsiteReader.java
@@ -0,0 +1,55 @@
+package com.cbfacademy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+//import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class WebsiteReader { // Class
+    public String message;
+/* Create a program that connects to the CBF academy website, then prints its content to the screen line by line.
+Whenever possible, use the try-with-resources construct we saw earlier in the course, and ensure to release all resources.*/
+
+    public void main (String[] args) throws Exception { // Throw exception method
+        try { 
+            URL myURL = new URL("https://codingblackfemales.com/"); // Defines the URL of the CBF academy website.
+            URLConnection myURLConnection = myURL.openConnection();      //Creates a URL object with the specified URL.
+            myURLConnection.connect();
+            /* You can wrap your InputStream in a InputStreamReader and can use it's read() method to read character 
+            data directly (note that you should specify the encoding when creating the Reader.*/
+
+           //String InputStreamReader = myURLConnection.getInputStream(); 
+            /* getInputStream(): The getInputStream() method of URLConnection returns an InputStream that allows the 
+            application to read from the URL resource. This method opens a connection to the URL if
+             it is not already open, retrieves the content, and returns an InputStream to read the content from.*/ 
+            
+            BufferedReader in = new BufferedReader(new InputStreamReader(myURL.openStream())); // BufferReader reads web content
+            String inputLine; //inputLine is just a variable used to store each line of text as it's read from the input stream within a loop. It's a common convention to use such a variable name when reading lines of input.
+                while ((inputLine = in.readLine()) != null)
+                System.out.println(inputLine);
+                in.close(); 
+        /*The close() method of Reader Class in Java is used to close the stream and release the resources that were busy 
+            in the stream, if any. This method has following results: If the stream is open, it closes the stream releasing the resources. 
+            If the stream is already closed, it will have no effect.*/
+        /*try (BufferedReader reader = new BufferedReader(new InputStreamReader(myURL.openStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);*/
+                } 
+                catch (MalformedURLException e) { // if the new URL failed. 
+                    System.out.println(e.getMessage()); 
+
+            } 
+            catch (IOException e) { // if opened connection failed.
+                System.out.println(e.getMessage());
+            
+            }
+    }
+}
+
+// e.printStackTrace()
+
+//https://docs.oracle.com/javase/tutorial/networking/urls/readingURL.html
--- exercises/java-exceptions/src/main/java/com/cbfacademy/FileExtension.java
+++ exercises/java-exceptions/src/main/java/com/cbfacademy/FileExtension.java
@@ -16,49 +16,44 @@ map -1 when an exception occurs*/
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 public class FileExtension {
-
-    public boolean check (String filename) throws FilenameException {
+      
+    public boolean check (String filename) throws FilenameException { // Constroctor for boolean with a throw 
         
-        if (filename == null || filename.isEmpty()){ // || represent OR
+        if (filename == null || filename.trim() == ""){ // if statement checking if the finename is || represent OR
 
-        throw new FilenameException( "File name is not java");
+        throw new FilenameException( "File name is not java or empty"); // throw exception statement
     
         } 
-        else if(filename.endsWith(".java")){
+        else if(filename.endsWith(".java")){ // if statement to check if the file ends with .java - 
             return true;
         } 
         else {
-            return false;
+            return false; // if not - return flase
         }
-
-        }
-        /*check each provided file's extension and map the returned value as 1 if true or 0 if falsemap -1 when an exception occurs
-        The map method should:
-        check each provided file's extension and map the returned value as 1 if true or 0 if false
-        map -1 when an exception occur*/
-
-        public Map<String, Integer> map(String filenames) {
+    }   
+        //The map method should:
+        //check each provided file's extension and map the returned value as 1 if true or 0 if false map -1 when an exception occur
+        
+        public Map<String, Integer> map(List<String> filenames) {
             Map<String, Integer> filenamesMap = new HashMap<>();
-
-           try{ 
-            for (String string: filenames){
-            //Map<String, Integer> filenamesMap;
-            if (check(string) == true){
-                filenamesMap.put(string, 1);
-            }
-            else{
-                filenamesMap.put(string, 0);
-            }
+        try{ 
+        for (String string : filenames) {  //check each provided file's extension and map the returned value as 1 if true or 0 if falsemap -1 when an exception occurs
+        
+        if (check(string) == true){
+            filenamesMap.put(string, 1);
         }
-           }
-            catch (FilenameException e) {
-                Map<String, Integer> result;
-                result.put(filenames, -1);
-                }
-            }
+        else{
+            filenamesMap.put(string, 0);
         }
+        }
+        }
+        catch (FilenameException e) {
+            filenamesMap.put(null, -1);
+        }
+            return filenamesMap;
+    }
+}
 
 
     
